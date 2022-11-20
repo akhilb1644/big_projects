9e9104d31316dcdc3dd4d51ec8f67864361a63b4
akhilmanhattan <48994987+akhilmanhattan@users.noreply.github.com>
2022-11-20 13:23:08 -0500

    Add files via upload

---
+++ CarryingCapacity(WithDisease)/nature.py
@@ -0,0 +1,6 @@
+class Cell:
+    def __init__(self,req_energy,rep_energy,eat_state,age):
+        self.req_energy = req_energy
+        self.rep_energy = rep_energy
+        self.eat_state = eat_state
+        self.age = age
