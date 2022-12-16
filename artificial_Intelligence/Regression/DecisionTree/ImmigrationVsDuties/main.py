0699b04a5cdf04e2dd757251e0c0e717efd78c05
Akhil B <48994987+akhilmanhattan@users.noreply.github.com>
2022-12-16 13:52:36 -0500

    Update main.py

--- artificial_Intelligence/Regression/DecisionTree/ImmigrationVsDuties/main.py
+++ artificial_Intelligence/Regression/DecisionTree/ImmigrationVsDuties/main.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split as tts
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeRegressor as dtr
+import matplotlib.pyplot as ppt
 
+data = pd.read_csv(r"iif.csv")
+x = data.iloc[:,:-1].values
+y = data.iloc[:,-1].values
+
+# Test Training Split
+xtn,xtt,ytn,ytt = tts(x,y,test_size=0.25,random_state=4)
+
+# Scaling the data
+stsclr = StandardScaler()
+sxtn,sxtt = stsclr.fit_transform(xtn),stsclr.transform(xtt)
