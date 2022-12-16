8ebc88579178e5c9063635d8e12557c49d3bd9a0
Akhil B <48994987+akhilmanhattan@users.noreply.github.com>
2022-12-16 13:55:16 -0500

    Update main.py

--- artificial_Intelligence/Regression/DecisionTree/ImmigrationVsDuties/main.py
+++ artificial_Intelligence/Regression/DecisionTree/ImmigrationVsDuties/main.py
@@ -1,3 +1,11 @@
+"""
+My sources for this Decision Tree Regressio's Data:
+
+https://www.minneapolisfed.org/about-us/monetary-policy/inflation-calculator/consumer-price-index-1913-
+https://fraser.stlouisfed.org/files/docs/publications/histstatus/pages/1975-1979/58477_1975-1979.pdf
+https://www.migrationpolicy.org/programs/data-hub/charts/immigrant-population-over-time
+"""
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split as tts
