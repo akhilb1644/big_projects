e5f633045254eb33cb552d445bb18d990b07a9fe
Akhil B <48994987+akhilmanhattan@users.noreply.github.com>
2023-01-13 09:30:51 -0500

    Create main.py

---
+++ artificial_Intelligence/Regression/DecisionTree/AmericaPrediction(1)/main.py
@@ -0,0 +1,12 @@
+"""
+My sources for this Decision Tree Regressio's Data:
+
+https://www.minneapolisfed.org/about-us/monetary-policy/inflation-calculator/consumer-price-index-1913-
+
+https://fraser.stlouisfed.org/files/docs/publications/histstatus/pages/1975-1979/58477_1975-1979.pdf
+
+https://www.migrationpolicy.org/programs/data-hub/charts/immigrant-population-over-time
+WARNING: Foreign Born Populations in unmarked years are estimations based on average rate of change between marked years.
+
+https://www.cdc.gov/nchs/data/statab/t1x0197.pdf
+"""
