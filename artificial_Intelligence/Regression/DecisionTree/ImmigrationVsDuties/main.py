f4863313308c7db4ef3017af742262847a38d976
Akhil B <48994987+akhilmanhattan@users.noreply.github.com>
2023-01-12 20:22:34 -0500

    Update main.py

--- artificial_Intelligence/Regression/DecisionTree/ImmigrationVsDuties/main.py
+++ artificial_Intelligence/Regression/DecisionTree/ImmigrationVsDuties/main.py
@@ -23,7 +23,7 @@ xtn,xtt,ytn,ytt = tts(x,y,test_size=0.25,random_state=4)
 
 # Making the model
 model = DecisionTreeRegressor(max_depth = 57)
-model.fit(xtn,ytn) # R-Squared value is 0.9697835849207701
+model.fit(xtn,ytn) # R-Squared value is 0.9695022095831883
 
 # I will make a graph for import duties (predicted) by this model from 1970 as data doesn't exist in the source
 topred = pd.read_csv(r"2pred.csv")
@@ -35,9 +35,19 @@ ppt.xlabel('Year')
 ppt.ylabel('Predicted Tariff Rate')
 ppt.plot(years,model.predict(topred),color='cyan')
 ppt.savefig('fig.png')
+ppt.clf()
 
 """
 As we can see, the model is not correct(as the 1990s and 2000s were times of extremely low tariffs). However, it gives a good insight into the future of 
 America as becoming more isolationist. I will update the model to be more accurate possibly by adding a religiousness percentage or some other statistic.
 I would also like to say that this machine learning model was pretty accurate on the data set, so it is a start.
 """
+
+# Making a graph from the actual data(no immigration because it is pretty much just a downward sloping straight line on the graph).
+ppt.title('Year vs Tariff & Inflation Rates')
+ppt.xlabel('Year')
+ppt.ylabel('Tariff & Inflation Rates')
+ppt.plot(list(range(1914,1971)),y,color='blue')
+ppt.plot(list(range(1914,1971)),x[:,:-1],color='red')
+ppt.legend(['Tariff Rate','Inflation Rate'])
+ppt.savefig('fig2.png')
