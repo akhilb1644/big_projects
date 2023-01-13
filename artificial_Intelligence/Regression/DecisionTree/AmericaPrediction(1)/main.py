d482e586a9ef9c93f2faf3b84be2ca9087bfc7a0
Akhil B <48994987+akhilmanhattan@users.noreply.github.com>
2023-01-13 17:25:54 -0500

    Update main.py

--- artificial_Intelligence/Regression/DecisionTree/AmericaPrediction(1)/main.py
+++ artificial_Intelligence/Regression/DecisionTree/AmericaPrediction(1)/main.py
@@ -10,3 +10,19 @@ WARNING: Foreign Born Populations in unmarked years are estimations based on ave
 
 https://www.cdc.gov/nchs/data/statab/t1x0197.pdf
 """
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split as tts
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeRegressor
+import matplotlib.pyplot as ppt
+
+data = pd.read_csv(r"totalFertility.csv")
+x = data.iloc[:,1:-1].values
+y = data.iloc[:,-1].values
+
+xtn,xtt,ytn,ytt = tts(x,y,test_size=0.25,random_state=4)
+
+model = DecisionTreeRegressor(max_depth = 57)
+model.fit(xtn,ytn) # Score of the model is 0.8894300622742276
